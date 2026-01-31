830a534251ae44c4444d26a8ba5eb4077276a5d1
Rishusingh18 <194272463+Rishusingh18@users.noreply.github.com>
2026-01-31 15:52:56 +0530

    Fix Google Auth redirect_uri

--- components/auth/google-auth-button.tsx
+++ components/auth/google-auth-button.tsx
@@ -20,7 +20,7 @@ export default function GoogleAuthButton({ nextUrl = "/" }: { nextUrl?: string }
             const { error } = await supabase.auth.signInWithOAuth({
                 provider: "google",
                 options: {
-                    redirectTo: `${window.location.origin}/auth/callback?next=${nextUrl}`,
+                    redirectTo: 'https://unispare.in/auth/callback',
                 },
             });
 
